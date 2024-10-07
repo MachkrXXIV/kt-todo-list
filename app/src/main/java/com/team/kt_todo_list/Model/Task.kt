2475f19d10dcebb3d301d19760c6f661ba9d6a4d
Justin Mach <bachmach24k@gmail.com>
2024-10-06 22:19:30 -0500

    add: basic task class

---
+++ app/src/main/java/com/team/kt_todo_list/Model/Task.kt
@@ -0,0 +1,11 @@
+package com.team.kt_todo_list.Model
+
+import java.util.Date
+
+class Task {
+    val id: Int = 0
+    val title: String = ""
+    val description: String = ""
+    val isCompleted: Boolean = false
+    val dueDate: Date = Date()
+}
