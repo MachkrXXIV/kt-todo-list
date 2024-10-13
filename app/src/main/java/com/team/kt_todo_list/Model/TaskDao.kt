7da6c820a1610e0f14132eb4d8e5ebb4a2ccf06f
Justin Mach <bachmach24k@gmail.com>
2024-10-13 18:27:23 -0500

    add: task dao

---
+++ app/src/main/java/com/team/kt_todo_list/Model/TaskDao.kt
@@ -0,0 +1,26 @@
+package com.team.kt_todo_list.Model
+
+import androidx.room.Dao
+import androidx.room.Insert
+import androidx.room.MapInfo
+import androidx.room.OnConflictStrategy
+import androidx.room.Query
+import androidx.room.Update
+import kotlinx.coroutines.flow.Flow
+
+@Dao
+interface TaskDao {
+
+    @MapInfo(keyColumn = "id")
+    @Query("SELECT * FROM task_table ORDER BY title ASC")
+    fun getAlphabetizedWords(): Flow<Map<Int,Task>>
+
+    @Update
+    suspend fun update(task: Task)
+
+    @Insert(onConflict = OnConflictStrategy.IGNORE)
+    suspend fun insert(task: Task)
+
+    @Query("DELETE FROM task_table")
+    suspend fun deleteAll()
+}
